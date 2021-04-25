a2cb8e194d967d3259944d2d3537e3d0c024af22
DaNicolescu <daniel.nicolescu10@gmail.com>
2021-04-25 18:46:15 +0300

    finished tee implementation

--- chapter_4/4.1/my_tee.c
+++ chapter_4/4.1/my_tee.c
@@ -3,13 +3,43 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUF_SIZE 1024
+
+int write_data(int fd, char *buffer, size_t n)
+{
+    size_t total_bytes_written = 0;
+    size_t crt_bytes_written;
+
+    while (total_bytes_written < n)
+    {
+        crt_bytes_written = write(fd, buffer + total_bytes_written,
+                                  n - total_bytes_written);
+
+        if (crt_bytes_written < 0)
+        {
+            perror("write");
+
+            return 1;
+        }
+
+        total_bytes_written += (size_t) crt_bytes_written;
+    }
+
+    return 0;
+}
+
 int read_data(char *file_name, int append_flag)
 {
     int fd;
     int flags = O_WRONLY | O_CREAT;
+    char buffer[BUF_SIZE];
+    ssize_t num_bytes_read;
+    int ret;
 
     if (append_flag)
         flags |= O_APPEND;
+    else
+        flags |= O_TRUNC;
 
     fd = open(file_name, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IRGRP
               | S_IROTH);
@@ -19,6 +49,33 @@ int read_data(char *file_name, int append_flag)
         perror("open");
 
         return 1;
+    }
+
+    while (1)
+    {
+        num_bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
+
+        if (num_bytes_read < 0)
+        {
+            perror("read");
+
+            return 1;
+        }
+        else if (num_bytes_read == 0)
+        {
+            break;
+        }
+
+        ret = write_data(STDOUT_FILENO, buffer, (size_t)num_bytes_read);
+
+        if (ret)
+            return 1;
+
+        ret = write_data(fd, buffer, (size_t)num_bytes_read);
+
+        if (ret)
+            return 1;
+
     }
 
     if (close(fd) != 0)
@@ -44,11 +101,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    while ((c = getopt (argc, argv, "l:")) != -1)
+    while ((c = getopt (argc, argv, "a:")) != -1)
     {
         switch (c)
         {
-        case 'l':
+        case 'a':
             append_flag = 1;
             file_name = optarg;
 
